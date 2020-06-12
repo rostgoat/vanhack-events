c2f595b407dbbdb508709167e1ea58d8cb99fad8
Rost <rdavidoff@hotmail.ca>
2020-06-12 11:39:09 -0700

    fixed problem with generating svg in js; stuff now renders

--- index.js
+++ index.js
@@ -1,32 +1,83 @@
-const events = [
-  {
-    id: 1,
-    title: "We still hacking amidst COVID19!",
-    date: "June 26, 2020",
-    location: "Vancouver, BC",
-    type: "vanhackathon",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fringilla nunc augue, non auctor leo mattis id. Curabitur ultrices ligula orci, ac iaculis neque tempor eu. Cras porta purus quis velit auctor, ac bibendum neque iaculis. Ut turpis ligula, semper ac lacinia hendrerit, rhoncus ut tortor. Donec tempor auctor orci sit amet consectetur. Vestibulum at mi ut erat volutpat pharetra convallis a tellus. Sed eget elit malesuada, tincidunt purus facilisis, laoreet mauris. Duis malesuada justo at nulla pretium congue. Fusce nec porttitor orci. Sed dapibus metus non laoreet pulvinar. Curabitur eget sodales purus. Donec at leo ut nulla elementum molestie. Curabitur ac consectetur lectus, quis vestibulum augue. Sed sed imperdiet urna.",
+const colors =
+{
+  hackathon: {
+    start: "#0675ce",
+    end: "#25c1cb",
   },
-];
+  leap: {
+    start: "#65de2b",
+    end: "#b1cf53",
+  },
+  mission: {
+    start: "#ff7b37",
+    end: "#FFBA36",
+  },
+  webinar: {
+    start: "#7106ce",
+    end: "#ba9c9c",
+  },
+  meetup: {
+    start: "#86a1f1",
+    end: "#b2ce06",
+  }
+}
 
+const icons = 
+{
+  hackathon: {
+    icon: "fa-laptop-code",
+    lg: "lg",
+    twoX: "2x"
+  },
+  leap: {
+    icon: "fa-frog",
+    lg: "lg",
+    twoX: "2x"
+  },
+  mission: {
+    icon: "fa-rocket",
+    lg: "lg",
+    twoX: "2x"
+  },
+  webinar: {
+    icon: "fa-user-friends",
+    lg: "lg",
+    twoX: "2x"
+  },
+  meetup: {
+    icon: "fa-handshake",
+    lg: "lg",
+    twoX: "2x"
+  }
+}
+
+
+var events = [];
 const eventsListHackathon = document.querySelector("#events-list__hackathon");
 const eventInfoButton = document.getElementById("event-info-button");
 
+/**
+ * Initialize all the data at the start of the application
+ */
 function init() {
+  console.log('events inside init', events)
   events.forEach(function (event) {
     createEventItem(event);
   });
 }
 
+/**
+ * Create event object in with and append it to DOM
+ * @param {Object} event Event Object
+ */
 function createEventItem(event) {
   const eventItem = document.createElement("div");
   eventItem.setAttribute("class", "events__item");
 
-  const header = createEventHeader();
-  const border = createEventBorder();
+  const header = createEventHeader(event);
+  const border = createEventBorder(event);
   const content = createEventContent(event);
-  const footer = createEventFooter();
+  const footer = createEventFooter(event);
 
   eventItem.appendChild(header);
   eventItem.appendChild(border);
@@ -36,7 +87,11 @@ function createEventItem(event) {
   eventsListHackathon.appendChild(eventItem);
 }
 
-function createEventHeader() {
+/**
+ * Create Event header
+ */
+function createEventHeader(event) {
+
   const eventItemHeader = document.createElement("div");
   eventItemHeader.classList.add(
     "events__item-header",
@@ -49,23 +104,16 @@ function createEventHeader() {
   const eventItemHeaderIcon = document.createElement("div");
   eventItemHeaderIcon.setAttribute(
     "class",
-    "events__item-icon-gradient--hackathon"
+    `events__item-icon-gradient--${event.type}`
   );
 
-  eventItemHeaderIcon.innerHTML = `
-                      <svg width="0" height="0">
-                        <radialGradient
-                          id="hackathon-rg"
-                          r="150%"
-                          cx="25%"
-                          cy="110%"
-                        >
-                          <stop stop-color="#0675ce" offset="0" />
-                          <stop stop-color="#25c1cb" offset="0.9" />
-                        </radialGradient>
-                      </svg>
-                      <i class="fas fa-laptop-code fa-2x"></i>
-  `;
+  const svg = createEventSVG(event);
+
+  const icon = document.createElement("i");
+  icon.classList.add("fas", `${event.icon.icon}`, `fa-${event.icon.twoX}`);
+
+  eventItemHeaderIcon.appendChild(svg);
+  eventItemHeaderIcon.appendChild(icon);
 
   eventItemHeaderWrapper.appendChild(eventItemHeaderIcon);
   eventItemHeader.appendChild(eventItemHeaderWrapper);
@@ -73,13 +121,22 @@ function createEventHeader() {
   return eventItemHeader;
 }
 
-function createEventBorder() {
+
+
+/**
+ * Create Event Border
+ */
+function createEventBorder(event) {
   const eventBorder = document.createElement("div");
-  eventBorder.classList.add("events__item-header-border", "lg--hackathon");
+  eventBorder.classList.add("events__item-header-border", `lg--${event.type}`);
 
   return eventBorder;
 }
 
+/**
+ * Create Event content
+ * @param {Object} event Event Object
+ */
 function createEventContent(event) {
   const eventContent = document.createElement("div");
   eventContent.setAttribute("class", "events__item-content");
@@ -116,41 +173,25 @@ function createEventContent(event) {
   return eventContent;
 }
 
-function createEventFooter() {
+/**
+ * Create Event Footer
+ */
+function createEventFooter(event) {
+  const svgURI = "http://www.w3.org/2000/svg";
+
   const eventFooter = document.createElement("div");
   eventFooter.setAttribute("class", "events__item-footer");
 
   const eventSocialMedia = document.createElement("div");
   eventSocialMedia.classList.add(
     "events__item-twitter",
-    "events__item-icon-gradient--hackathon"
+    `events__item-icon-gradient--${event.type}`
   );
 
-  const svg = document.createElement("svg");
-  svg.setAttribute("width", "0");
-  svg.setAttribute("height", "0");
-
-  const radialGradient = document.createElement("radialGradient");
-  radialGradient.setAttribute("id", "hackathon-rg");
-  radialGradient.setAttribute("r", "150%");
-  radialGradient.setAttribute("cx", "25%");
-  radialGradient.setAttribute("cy", "110%");
-
-  const stop = document.createElement("stop");
-  stop.setAttribute("stop-color", "#0675ce");
-  stop.setAttribute("offset", "0");
-
-  const stop2 = document.createElement("stop");
-  stop2.setAttribute("stop-color", "#25c1cb");
-  stop2.setAttribute("offset", "0.9");
+  const svg = createEventSVG(event)
 
   const icon = document.createElement("i");
-  icon.classList.add("fab", "fa-twitter", "fa-lg");
-
-  radialGradient.appendChild(stop);
-  radialGradient.appendChild(stop2);
-
-  svg.appendChild(radialGradient);
+  icon.classList.add("fas", `${event.icon.icon}`, `fa-${event.icon.lg}`);
 
   eventSocialMedia.appendChild(svg);
   eventSocialMedia.appendChild(icon);
@@ -161,7 +202,7 @@ function createEventFooter() {
   const eventDetailsButton = document.createElement("button");
   eventDetailsButton.classList.add(
     "events__item-details-button",
-    "events__item-details-button--hackathon"
+    `events__item-details-button--${event.type}`
   );
   eventDetailsButton.setAttribute("id", "event-info-button");
   eventDetailsButton.appendChild(document.createTextNode("Details"));
@@ -174,17 +215,86 @@ function createEventFooter() {
   return eventFooter;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  eventInfoButton.addEventListener('click', function(e) {
-    e.preventDefault();
-  
-    console.log('clicked')
-  })
-});
-if (eventInfoButton) {
-  
-}
-window.onload = function() {
+function createEventSVG(event) {
+  const svgURI = "http://www.w3.org/2000/svg";
+
+  const svg = document.createElementNS(svgURI, "svg");
+  svg.setAttribute("width", "0");
+  svg.setAttribute("height", "0");
+
+  const radialGradient = document.createElementNS(svgURI, "radialGradient");
+  radialGradient.id = `${event.type}-rg`;
+  radialGradient.setAttribute("r", "150%");
+  radialGradient.setAttribute("cx", "25%");
+  radialGradient.setAttribute("cy", "110%");
+
+  const stop = document.createElementNS(svgURI, "stop");
+  stop.setAttribute("stop-color", event.color.colorStart);
+  stop.setAttribute("offset", "0");
+
+  const stop2 = document.createElementNS(svgURI, "stop");
+  stop2.setAttribute("stop-color", event.color.colorEnd);
+  stop2.setAttribute("offset", "0.9");
+
+  radialGradient.appendChild(stop);
+  radialGradient.appendChild(stop2);
+
+  svg.appendChild(radialGradient);
+
+  return svg;
 }
 
-init();
+/**
+ * Setup event listeners when application is loading to prevent
+ * them from throwing null errors
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('were loading stuff?')
+  // eventInfoButton.addEventListener('click', (e) => {
+  //   e.preventDefault();
+
+  //   console.log('clicked')
+  // })
+
+  const loadData = () => {
+    if (typeof colors !== 'undefined') {
+      events = [
+        {
+          id: 0,
+          title: "We still hacking amidst COVID19!",
+          date: "June 26, 2020",
+          location: "Vancouver, BC",
+          type: "hackathon",
+          color: {
+            colorStart: colors.hackathon.start,
+            colorEnd: colors.hackathon.end
+          },
+          icon: icons.hackathon,
+          content:
+            ``,
+        },
+        {
+          id: 1,
+          title: "Let's take a leap into this new adventure!",
+          date: "June 30, 2020",
+          location: "Toronto, ON",
+          type: "leap",
+          color: {
+            colorStart: colors.leap.start,
+            colorEnd: colors.leap.end
+          },
+          icon: icons.leap,
+          content:
+            ``,
+        },
+      ];
+    } else {
+      window.setTimeout(loadData, 250);
+      window.setTimeout(init, 250);
+    }
+  };
+  // Initialize
+  loadData();
+  // run this function when application first loads in the DOM
+  init();
+});
